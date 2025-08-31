8b247c2f42fb578552057e720042b4438d5ce94a
Jaybie1909 <jaybie1909@gmail.com>
2025-08-31 17:29:49 +0800

    update google search

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="EqKob9SJXCDL9GkcKtShcF9-aO0Ytcu8cHTABT-Clm8" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
